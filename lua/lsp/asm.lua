e26aea54a23d0d5499f22aedc0d52d5f4894464b
EzT32 <theodor.berghansen@icloud.com>
2025-10-22 09:22:21 +0200

    fix: assembly config for nvim feat: add module for hyprsunset

--- lua/lsp/asm.lua
+++ lua/lsp/asm.lua
@@ -5,10 +5,12 @@ function M.setup()
   local shared = require('lsp.shared')
   local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
-  local capabilities = cmp_nvim_lsp.default_capailities()
+  local capabilities = cmp_nvim_lsp.default_capabilities()
   
   lspconfig.asm_lsp.setup({
     on_attach = shared.on_attach,
     capabilities = capabilities,
   })
 end
+
+return M
