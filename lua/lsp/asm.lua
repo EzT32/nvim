bae78d0301eef960219660384142309db33fd0c4
EzT32 <theodor.berghansen@icloud.com>
2026-02-14 14:26:22 +0100

    feat: asm formatting

--- lua/lsp/asm.lua
+++ lua/lsp/asm.lua
@@ -5,15 +5,25 @@ function M.setup()
   local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
   local capabilities = cmp_nvim_lsp.default_capabilities()
-  
+
 
   vim.lsp.config("asm_lsp", {
     on_attach = shared.on_attach,
-    capabilities = capabilities
+    capabilities = capabilities,
+    filetypes = { "asm", "s" }
   })
 
 
   vim.lsp.enable("asm_lsp")
+
+  local null_ls = require("null-ls")
+
+  null_ls.setup({
+    sources = {
+      null_ls.builtins.formatting.asmfmt,
+    },
+    on_attach = shared.on_attach,
+  })
 end
 
 return M
